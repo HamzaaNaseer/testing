993b6e5f9c446704258ad315a5bd0a1cd6535dd7
Hamza Naseer <hamzanaseer711@gmail.com>
2025-09-24 16:22:15 +0500

    testing styles

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const page = ({ params }) => {
-  return <div>this is the {params?.slug}</div>;
+  return <div className="text-orange-700 text-2xl bg-black">this is the {params?.slug}</div>;
 };
 
 export default page;
